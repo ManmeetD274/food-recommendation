40e61130d26492f321d93748ffce3d0aa6e8ec3b
Manmeet Deswal <manmeetdeswal274@gmail.com>
2022-09-07 08:10:08 +0530

    Initial commit

---
+++ src/main/java/com/foodrecommendation/app/FoodRecommendationAppApplication.java
@@ -0,0 +1,13 @@
+package com.foodrecommendation.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodRecommendationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodRecommendationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodrecommendation/app/FoodRecommendationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodrecommendation.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodRecommendationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
